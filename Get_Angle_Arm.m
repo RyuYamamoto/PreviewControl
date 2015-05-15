3a9ce783f7831124112b1e7cb8dd32839db2ed90
RyuYamamoto <ron_robotics@yahoo.co.jp>
2015-05-15 21:25:23 +0900

    Folder Arrangment

--- Get_Angle_Arm.m
+++
@@ -1,8 +0,0 @@
-function Angle = Get_Angle_Arm()
-    load('data/ServoID.mat');
-    
-    d1 = GetServoAngle(ElbowR);
-    d2 = GetServoAngle(ElbowL);
-    
-    Angle = [d1 d2];
-end
--- robot_status_init.m
+++
@@ -1,5 +0,0 @@
-function robot_status_init()
-    Init_PreviewControl_Paramter();
-    Init_Walk_Parameter();
-    ServoID();
-end
